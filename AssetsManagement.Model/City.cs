058a88bed0d0722ea4b7393b6c5cd4aa120122b1
scooter <eran.hadar@gmail.com>
2020-07-04 18:36:37 +0300

    change target version to .Net 4.8

---
+++ AssetsManagement.Model/City.cs
@@ -0,0 +1,13 @@
+﻿namespace AssetsManagement.Model
+{
+    public class City
+    {
+        public int Symbol { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"City: [Symbol: {Symbol}, Name: {Name}]";
+        }
+    }
+}
---
+++ AssetsManagement.Model/Tenant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AssetsManagement.Model
+{
+    public class Tenant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
